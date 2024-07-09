8ac54b2896eb69b9f7af6eef52a41cc3aae55181
sdmken <cafe223kenji@yahoo.co.jp>
2024-07-09 21:50:29 +0900

    Initial Commit

---
+++ 48/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  48
+//
+//  Created by 堤健二 on 2024/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
